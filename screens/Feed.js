04dd3f673ef3fe50e9032b5e5fc5b924bce763d6
System Administrator <root@Philips-MBP-2.cust.communityfibre.co.uk>
2023-12-02 09:29:11 +0000

    third commit

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,44 +1,72 @@
-import React, { Component } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { RFValue } from 'react-native-responsive-fontsize';
+import React, { Component } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export default class Feed extends Component{
-  render() {
-    return (
-        <View style={styles.container}>
-          <Text>Feed</Text>
-          <StatusBar style="auto" />
-        </View>
-      );
-    }
-  }
+import posts from "./PostCard";
+
+export default class Feed extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<SafeAreaView style={styles.droidSafeArea} />
+				<View style={styles.appTitle}>
+					<View style={styles.appIcon}>
+						<Image
+						source={require("../assets/splash.png")}
+						style={styles.iconImage}
+						></Image>
+					</View>
+					<View style={styles.appTitleTextContainer}>
+						<Text style={styles.appTitleText}>Spectagram</Text>
+					</View>
+				</View>
+				<View style={styles.cardContainer}>
+					<FlatList
+						keyExtractor={this.keyExtractor}
+						data={posts}
+						renderItem={this.renderItem}
+					/>
+				</View>
+			</View>
+			
+		);
+	}
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   droidSafeArea: {
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
   },
   appTitle: {
     flex: 0.07,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   appIcon: {
     flex: 0.2,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   iconImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
+    width: 250,
+    height: 250,
   },
   appTitleTextContainer: {
     flex: 0.8,
-    justifyContent: "center"
+    justifyContent: "center",
   },
   appTitleText: {
     color: "white",
@@ -46,5 +74,5 @@ const styles = StyleSheet.create({
   },
   cardContainer: {
     flex: 0.85,
-  }
+  },
 });
